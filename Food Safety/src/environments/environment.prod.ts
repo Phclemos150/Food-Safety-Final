efdfe7739ca4ba8633e31198822493c11f5117bf
Phclemos150 <166515056+Phclemos150@users.noreply.github.com>
2025-06-21 01:11:25 -0300

    Add files via upload

---
+++ Food Safety/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  myMemoryApiKey: 'c066d58f740eec6fe8fc',
+  firebaseConfig: {
+    apiKey: "AIzaSyC7El6f_Ef-WIYRodaoDoNQy1tl6LyKjjk",
+    authDomain: "food-safety-2c53d.firebaseapp.com",
+    projectId: "food-safety-2c53d",
+    storageBucket: "food-safety-2c53d.appspot.com",
+    messagingSenderId: "294396070534",
+    appId: "1:294396070534:web:78548ad8bfe635d63c5f07",
+    measurementId: "G-RKEB1HJJZR"
+  }
+};
