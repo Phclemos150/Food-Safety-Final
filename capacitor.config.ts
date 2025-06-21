6c762fefa465ce2923a13c1f12aa667e65157662
Phclemos150 <166515056+Phclemos150@users.noreply.github.com>
2025-06-21 01:03:47 -0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.nicollas.meuapp',
-  appName: 'meuAppTestes',
-  webDir: 'www'
-};
-
-export default config;
